47f84b6487f9a4e8be316dc79ca1709429a7d6e6
namhoaingo <namhoaingo@gmail.com>
2022-03-16 08:40:16 +0800

    Add Static field and stop watich

---
+++ StaticField/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+
+namespace StaticField
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("100 time class without static") ;
+            for (var i = 0; i < 100; i++)
+            {
+                var nonStaticClass = new NormalClassWithoutStatic();
+                Console.WriteLine("nonStaticClass Name: "+ nonStaticClass.Name);
+            }
+
+            Console.WriteLine("100 time class with static");
+            for (var i = 0; i < 100; i++)
+            {
+                var nonStaticClass = new NormalClassWithStatic();
+                Console.WriteLine("normal class with static Name: " + NormalClassWithStatic.Name);
+            }
+
+
+            Console.WriteLine("100 time static class with static");
+            for (var i = 0; i < 100; i++)
+            {
+                Console.WriteLine("static class Name: " + StaticClass.Name);
+            }
+        }
+    }
+
+    class NormalClassWithoutStatic
+    {
+        public string Name { get; } = Guid.NewGuid().ToString();
+    }
+
+
+    class NormalClassWithStatic
+    {
+        // Get keyword return the same instance
+        //public static string Name { get; } = Guid.NewGuid().ToString();
+
+        // => return different instance
+        public static string Name => Guid.NewGuid().ToString();
+
+    }
+
+    static class StaticClass
+    {
+        public static string Name { get; } = Guid.NewGuid().ToString();
+    }
+}
---
+++ Stopwatch/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace Stopwatch
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Calculate performance");
+
+            var stopWatch = new System.Diagnostics.Stopwatch();
+            stopWatch.Start();
+            System.Threading.Thread.Sleep(100);
+            stopWatch.Stop();
+            Console.WriteLine("Elapse time 1 --" + stopWatch.ElapsedMilliseconds);
+
+            System.Threading.Thread.Sleep(200);
+
+            stopWatch.Start();
+            System.Threading.Thread.Sleep(200);
+            stopWatch.Stop();
+
+            Console.WriteLine("Elapse time 2 --" + stopWatch.ElapsedMilliseconds); 
+
+        }
+    }
+}
