8d3356aadb625474a5ea1d49542b068e3e7f9614
NamhoaiNgo <NamhoaiNgo@Melaleuca.com>
2022-07-21 11:32:05 +0800

    Add Date time, Async Local, and Statis Async Local

---
+++ DateTime/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace DateTimeTest
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            var today = DateTime.Today;
+            var currentMoth = new DateTime(today.Year, today.Month, 1);
+
+            // Minus 1
+            var minus1 = currentMoth.AddMonths(-1);
+        }
+    }
+}
---
+++ StaticAsyncLocalvsStaticThread/Program.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace StaticAsyncLocalVsStaticThread
+{
+    public static class Program
+    {
+        //[ThreadStatic]
+        //public static string Ts;
+
+        // Nomal static
+        public static string Ts;
+
+        public static AsyncLocal<string> Al = new AsyncLocal<string>();
+
+        public static void Main(string[] args)
+        {
+            // Start 1 first context
+            Task.Run(async () =>{
+                Ts = "thread1";
+                Al.Value = "11";
+                await doStuff();
+            });
+
+            Console.ReadLine();
+            //print();
+            // Start 2 first context
+            Task.Run(async () => {
+                Ts = "thread2";
+                Al.Value = "22";
+                await doStuff();
+            });
+            Console.ReadLine();
+
+            //print();
+        }
+
+        private static async Task doStuff()
+        {
+         //   print();
+
+            Task t = Task.Run(async () =>
+            {
+                await Task.Delay(1000);
+                print();
+            });
+
+           // print();
+
+            await t;
+
+            //print();
+        }
+
+        private static void print()
+        {
+            Console.WriteLine("Managed Thread ID {0}", Thread.CurrentThread.ManagedThreadId);
+            Console.WriteLine("TS: {0}", Ts ?? "null");
+            Console.WriteLine("AL: {0}", Al.Value ?? "null");
+        }
+    }
+}
