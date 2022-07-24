0bbea30b547f69967aa0f7b830ba979197c56ae6
namhoaingo <namhoaingo@gmail.com>
2022-07-24 10:21:43 +0800

    Add ValidAnagram

---
+++ LeetCode/ValidAnagram.ts
@@ -0,0 +1,41 @@
+//https://leetcode.com/problems/valid-anagram/
+function isAnagram(s: string, t: string): boolean {
+
+    var size_plus = s.length;
+    var size_minus = t.length;
+
+    if (size_plus != size_minus) return false;
+    var dict = {}
+    for (var index = 0; index < size_plus; index++) {
+        var plus_chart_at_index = s.charAt(index);
+        var minus_chart_at_index = t.charAt(index);
+
+        if (dict[plus_chart_at_index]) {
+            // if plus 
+            dict[plus_chart_at_index] = dict[plus_chart_at_index] + 1;
+        }
+        else {
+            dict[plus_chart_at_index] = 1;
+        }
+
+        if (dict[minus_chart_at_index]) {
+            dict[minus_chart_at_index] = dict[minus_chart_at_index] - 1;
+        }
+        else {
+            dict[minus_chart_at_index] = -1;
+        }
+    }
+    // Get the result sum
+    
+    for (let key in dict){
+        if (dict[key] > 0){
+            return false;
+        }
+    }
+
+    return true;
+};
+
+
+//console.log(isAnagram("rat", "cat"));
+//console.log(isAnagram("anagram", "nagaram"));
