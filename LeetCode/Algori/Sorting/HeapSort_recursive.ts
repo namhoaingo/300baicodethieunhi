34a9386a51270082d5cb73406c2da4aea77e10ce
namhoaingo <namhoaingo@gmail.com>
2022-07-28 21:42:11 +0800

    Add HeapSort Recursive va longest consec w sort

---
+++ LeetCode/Algori/Sorting/HeapSort_recursive.ts
@@ -0,0 +1,48 @@
+function heapSortRecursive(inputs: number[]){
+    var current_last_index = inputs.length-1;
+    for(var j = 0; j< inputs.length; j++){
+        for(var i = current_last_index; i >= 0; i--){
+            // correct
+            heapSort(inputs, i, current_last_index);    
+            // Neu doi current_last_index thanh inputs.length
+            // thi duoc array tu to den nho 
+        }
+        swap_for_heapsort_recursive(inputs, 0, current_last_index);
+        current_last_index = current_last_index-1;    
+    }
+}
+
+function heapSort(inputs: number[], currentIndex: number, lastIndex: number){
+    var leftIndex = currentIndex * 2 + 1;
+    var rightIndex = currentIndex * 2 + 2;
+
+    // Check the range
+    if(Math.max(leftIndex, rightIndex) <= lastIndex){
+        if( inputs[currentIndex] < Math.max(inputs[leftIndex], inputs[rightIndex])){
+            if(inputs[rightIndex] > inputs[leftIndex]){
+                swap_for_heapsort_recursive(inputs, currentIndex, rightIndex);
+                heapSort(inputs, rightIndex, lastIndex);
+            }
+            else if(inputs[currentIndex] < inputs[leftIndex]){
+                swap_for_heapsort_recursive(inputs, currentIndex, leftIndex);
+                heapSort(inputs, leftIndex, lastIndex)
+            }             
+        } 
+    }else if(leftIndex <= lastIndex && inputs[currentIndex] < inputs[leftIndex]){
+        swap_for_heapsort_recursive(inputs, currentIndex, leftIndex);
+        heapSort(inputs, leftIndex, lastIndex);
+    } else if(rightIndex <= lastIndex && inputs[currentIndex] < inputs[rightIndex]){
+        swap_for_heapsort_recursive(inputs, currentIndex, rightIndex);
+        heapSort(inputs, rightIndex, lastIndex);
+    }
+}
+
+function swap_for_heapsort_recursive(inputs: number[], first_index: number, second_index: number){
+    var temp = inputs[first_index];
+    inputs[first_index] = inputs[second_index];
+    inputs[second_index] = temp;
+}
+
+var inputs = [2,1,8,9,10,0];
+heapSortRecursive(inputs);
+console.log(inputs);
---
+++ LeetCode/Array_Hashing/LongestConsecutiveSequence_diction_timeout.ts
@@ -0,0 +1,75 @@
+//https://leetcode.com/problems/longest-consecutive-sequence/
+/**
+ * Understand the problem
+ * We will need to sort the array some how. 
+ * The requirement does not specify space complexity, so we can use a big list to take care of this
+ * Nhưng vấn đề là --- con số đó có thể là số âm
+ *  -- Giải quyết số âm - số âm là 1 dãy
+ * -- Giải quyết số dương - số dương là 1 dãy
+ * có 3 trường hợp
+ * - Kiểm tra số dương
+ * - Kiêm tra số âm
+ * - kiểm tra nơi giao nhau giữa hai dãy
+ * 
+ * Time Complexity
+ * Loop qua array một vòng O(n);
+ * Loop qua array kết quả một vòng nữa - Vấn đề: Nếu con số quá lớn, thì nó sẽ 
+ * gấp nhiều lần n
+ * 
+ * Chẳng lẽ lại phải dùng sort à
+ * -- Nhưng cái khác là consecutive, không biết nó có cho mình cái hint gì ko đây
+ * 
+ * 
+ * 
+ * *********************************************************************************
+ * Cách khác
+ * https://leetcode.com/problems/longest-consecutive-sequence/discuss/41057/Simple-O(n)-with-Explanation-Just-walk-each-streak
+ * Cách này khác khá hay, có liên quan đến sử dụng dictionary 
+ * 1. Loop qua tất cả các giá trị trong array
+ *  1.1 - Đưa vào trong dictionary
+ * 
+ * 2. Loop qua tất cả các giá trị trong array lần nữa
+ *  2.1 - công lên dần và kiểm trả xem trong dict có không
+ *  2.2 - Length là bằng điểm dừng trừ đi số hiện tại
+ * 
+ * Time Complexity 
+ * Loop qua array lần 1 - O(n) - Tạo dict
+ * Loop qua array lần 2 - O(n)
+ *      Trong khi loop thì kiểm tra xem có trong dict ko O(1)
+ *      Phải kiểm tra trong n lần
+ *      ==> O(n)
+ * 
+ * Kết quả O(n^2)
+ * 
+ * Space Complexity 
+ * Sữ dựng Dict có size bằng n O(n)
+ */
+
+
+function longestConsecutive(nums: number[]): number {
+    var dict = {};
+    var max_consecutive_length = 0;
+    for (var i = 0; i < nums.length; i++){
+        if(!dict[nums[i]]){
+            dict[nums[i]] = 1;
+        }
+    }
+
+    for (var j = 0; j < nums.length; j++){
+        var starting_num = nums[j];
+        while (dict[starting_num]){
+            starting_num = starting_num + 1;
+        }
+
+        var current_count = starting_num - nums[j];
+        if(current_count >max_consecutive_length){
+            max_consecutive_length = current_count;
+        }
+    }
+
+    return max_consecutive_length;
+};
+
+//console.log(longestConsecutive([100,4,200,1,3,2]));
+
+// Cach nay co ket qua time out
