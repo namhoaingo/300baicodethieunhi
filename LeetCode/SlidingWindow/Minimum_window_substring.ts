693a36bb4cfd6f3278403adfd4e893b27f6d8850
NamhoaiNgo <NamhoaiNgo@Melaleuca.com>
2022-08-04 11:31:06 +0800

    Get in Min window substring bad performance

--- LeetCode/SlidingWindow/Minimum_window_substring.ts
+++ LeetCode/SlidingWindow/Minimum_window_substring.ts
@@ -4,50 +4,67 @@ function minWindow(s: string, t: string): string {
     // neu bad, move right
 
     var t_dict = {};
+    var s_dict  = {};
     for(var i = 0; i < t.length; i++){
         if(t_dict[t[i]] == undefined){
-            t_dict[t[i]] = 0;
+            t_dict[t[i]] = 1;
         }else{
             t_dict[t[i]] = t_dict[t[i]] + 1;
         }
+        // Set s_dict to 0
+        s_dict[t[i]] = 0;        
     }
 
     var isMatched = false;
     var left = 0;
-    var s_dict  = {};
     var current_inspect_index  = 0;
-    var current_max_left  = -1;
-    var current_min_right = -1;
-    for(var right = 0; right < s.length; right++){
-        
+    var current_found_left  = -1;
+    var current_found_right = -1;
+    var min_length = -1;
+    for(var right = 0; right <= s.length; right++){
         if(isMatched == false){
             // Them vao
             current_inspect_index = right;
-            if(t_dict[s[current_inspect_index]] != undefined){
+            if(s_dict[s[current_inspect_index]] != undefined){
                 // Char nay co trong t
-                // Cho vao s_dict
-                s_dict[s[current_inspect_index]] == undefined 
-                    ? s_dict[s[current_inspect_index]] = 0
-                    : s_dict[s[current_inspect_index]] = s_dict[s[current_inspect_index]] + 1;
+                // Cho vao s_dict                
+                s_dict[s[current_inspect_index]] = s_dict[s[current_inspect_index]] + 1;
                 // determine the is Match
                 isMatched = determind_is_match(s_dict, t_dict);
             }
         }
         else{
-            current_max_left = Math.max(left, current_max_left);
-            current_min_right = Math.min(right, current_min_right);
+            var length = right - left;
+            if(min_length == -1){
+                min_length = length;
+                current_found_left = left;
+                current_found_right = right;
+            }
+            else if(min_length > length){
+                min_length = length;
+                // Only recalculate left and right after isMatch is true
+                current_found_left = left;
+                current_found_right = right;
+            }
+            
             // bo di
             current_inspect_index = left;
             // check dict
-            if(t_dict[s[current_inspect_index]] != undefined){
+            // if s_dict contain this key, we need to remove it
+            if(s_dict[s[current_inspect_index]] != undefined){
                 // t contain the char ---> change s_dict
-                s_dict[s[current_inspect_index]] == undefined 
-                    ? s_dict[s[current_inspect_index]] = 0
-                    : s_dict[s[current_inspect_index]] = s_dict[s[current_inspect_index]] + 1;
+                s_dict[s[current_inspect_index]] = s_dict[s[current_inspect_index]]-1;
             }
+            isMatched = determind_is_match(s_dict, t_dict);
+            left = left + 1;
+            // dont change right
+            right = right - 1;
         }
-
     }
+    if(current_found_right >= 0 && current_found_left >= 0){
+        return s.substring(current_found_right, current_found_left);
+    }
+    return "";
 };
 
 
@@ -59,3 +76,9 @@ function determind_is_match(s_dict: Object, t_dict: Object): boolean{
     }
     return true;
 }
+
+console.log(minWindow("cabwefgewcwaefgcf","cae"));
+
+
+// Runtime: 515 ms, faster than 8.90% of TypeScript online submissions for Minimum Window Substring.
+// Memory Usage: 52.8 MB, less than 9.42% of TypeScript online submissions for Minimum Window Substring.
