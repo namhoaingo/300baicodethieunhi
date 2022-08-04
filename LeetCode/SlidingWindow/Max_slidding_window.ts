60c780f63445b95e68b2527cf838bc52c9f85937
NamhoaiNgo <NamhoaiNgo@Melaleuca.com>
2022-08-04 17:13:25 +0800

    Start Max Sliding

---
+++ LeetCode/SlidingWindow/Max_slidding_window.ts
@@ -0,0 +1,25 @@
+//https://leetcode.com/problems/sliding-window-maximum/
+function maxSlidingWindow(nums: number[], k: number): number[] {
+    var result = new Array();
+    var current_max = 0; 
+    var current_second_max = 0;
+    var left = 0; 
+    var right = 0; 
+    for(var right = 0; right < nums.length; right++){
+        // ko chay den so cuoi
+        if(right-left < k - 1){
+            // Minh muon process cai right
+            current_max = Math.max(nums[right], current_max);
+        }
+        else if(right - left == k-1){
+            result.push(current_max);
+        }
+        else{            
+            // move the left
+            left = left + 1;
+            if(nums[left] < nums[right]){
+                current_max = nums[right];
+            }
+        }
+    }
+};
