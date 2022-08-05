97005431f42c0866723bd7643259e66880f2aa6d
namhoaingo <namhoaingo@gmail.com>
2022-08-05 08:20:29 +0800

    Delete everything for maxSliding

--- LeetCode/SlidingWindow/Max_slidding_window.ts
+++ LeetCode/SlidingWindow/Max_slidding_window.ts
@@ -1,27 +1,15 @@
 //https://leetcode.com/problems/sliding-window-maximum/
-function maxSlidingWindow(nums: number[], k: number): number[] {
-    var result = new Array();
-    var current_max = 0; 
-    var current_second_max = 0;
-    var left = 0; 
-    var right = 0; 
-    for(var right = 0; right < nums.length; right++){
-        // ko chay den so cuoi
-        if(right-left < k - 1){
-            // Minh muon process cai right
-            current_max = Math.max(nums[right], current_max);
-        }
-        else if(right - left == k-1){
-            result.push(current_max);
-        }
-        else{            
-            // move the left
-            left = left + 1;
-            if(nums[left] < nums[right]){
-                current_max = nums[right];
-            }
-        }
 
-        return new Array();
-    }
+// Solve this by using a queue
+// each move we will 
+    //1. Take a number out
+    //2. Add a number in
+//1. When take a number out, if it is bigger or equal the biggest. remove the 
+// biggest. 
+    // check the number, remove 
+//2. 
+function maxSlidingWindow(nums: number[], k: number): number[] {
+    
+    
+    return new Array();
 };
