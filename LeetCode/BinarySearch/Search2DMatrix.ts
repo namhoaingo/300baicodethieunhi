60ea028fb9197e44a3632f183245947072287835
NamhoaiNgo <NamhoaiNgo@Melaleuca.com>
2022-08-09 14:33:28 +0800

    add result for Search 2d

--- LeetCode/BinarySearch/Search2DMatrix.ts
+++ LeetCode/BinarySearch/Search2DMatrix.ts
@@ -48,3 +48,7 @@ function BinarySearch2DMatrix(numbers: number[], target: number): boolean{
 }
 
 console.log(searchMatrix([[1,3,5,7],[10,11,16,20],[23,30,34,60]], 3));
+
+
+//Runtime: 76 ms, faster than 84.62% of TypeScript online submissions for Search a 2D Matrix.
+//Memory Usage: 44.8 MB, less than 15.85% of TypeScript online submissions for Search a 2D Matrix.
