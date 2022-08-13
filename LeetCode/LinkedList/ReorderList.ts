1607b10441bb34d68e927bdbd2884dd7270b637d
namhoaingo <namhoaingo@gmail.com>
2022-08-13 10:47:02 +0800

    finish re order list

--- LeetCode/LinkedList/ReorderList.ts
+++ LeetCode/LinkedList/ReorderList.ts
@@ -48,7 +48,37 @@ class ContainerListNodeReorderList{
 }
 
 function reorderList(head: ListNodeReoderList | null): void {
-   
+   let slowPointer = head;
+   let fastPointer = head;
+
+   let originalHead = head;
+   //loop until the end
+   while(fastPointer != null && fastPointer.next != null){
+    slowPointer = slowPointer.next;
+    fastPointer = fastPointer.next.next;
+   }
+
+   // start flipping
+   let temp = null; 
+   let prev = null; 
+   while(slowPointer){
+    temp = slowPointer.next;
+    slowPointer.next = prev;
+    prev = slowPointer;
+    slowPointer = temp;
+   }
+
+
+   // merge the array
+   while(originalHead.next && prev.next){
+    let temp2 = originalHead.next
+    let temp3 = prev.next
+    originalHead.next = prev
+    prev.next = temp2;
+    originalHead = temp2;
+    prev = temp3
+   }
+
 };
 
 var test = new ContainerListNodeReorderList();
@@ -58,5 +88,10 @@ test.add(3);
 test.add(4);
 test.add(5);
 test.add(6);
+//test.add(7);
 
 reorderList(test.head);
+
+
+//Runtime: 95 ms, faster than 98.31% of TypeScript online submissions for Reorder List.
+//Memory Usage: 50.8 MB, less than 70.62% of TypeScript online submissions for Reorder List.
