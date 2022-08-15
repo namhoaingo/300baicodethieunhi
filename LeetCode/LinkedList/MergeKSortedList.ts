ea943b27cd5d030509d94f47379c9acaa8a85d31
namhoaingo <namhoaingo@gmail.com>
2022-08-15 21:42:01 +0800

    Add LRUCache draw

---
+++ LeetCode/LinkedList/MergeKSortedList.ts
@@ -0,0 +1,16 @@
+//https://leetcode.com/problems/merge-k-sorted-lists/
+/**
+ * Definition for singly-linked list.
+ * class ListNode {
+ *     val: number
+ *     next: ListNode | null
+ *     constructor(val?: number, next?: ListNode | null) {
+ *         this.val = (val===undefined ? 0 : val)
+ *         this.next = (next===undefined ? null : next)
+ *     }
+ * }
+ */
+
+function mergeKLists(lists: Array<ListNode | null>): ListNode | null {
+
+};
