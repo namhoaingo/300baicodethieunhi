097577754b02ce74ab01e127cedc7a330a94bf21
namhoaingo <namhoaingo@gmail.com>
2022-08-15 23:05:10 +0800

    add merge K Sorted List Bruce Force

--- LeetCode/LinkedList/MergeKSortedList_Bruce_Force.ts
+++ LeetCode/LinkedList/MergeKSortedList_Bruce_Force.ts
@@ -12,5 +12,18 @@
  */
 
 function mergeKLists(lists: Array<ListNode | null>): ListNode | null {
+    // 
 
 };
+
+function mergeSort_linkedList(start: number, end: number, lists: Array<ListNode | null>): ListNode{
+    let mid = Math.floor((start + end)/2);
+    let left = mergeSort_linkedList(start, mid, lists);
+    let right = mergeSort_linkedList(mid, end, lists);
+    let result = merge_linkedList(left, right);
+    return result;
+}
+
+function merge_linkedList(l1: ListNode, l2: ListNode): ListNode{
+
+}
