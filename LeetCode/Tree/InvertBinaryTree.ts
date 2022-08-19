7bacdd8964613d5d01d78106ce4d1bfbbd5821ef
namhoaingo <namhoaingo@gmail.com>
2022-08-19 23:19:40 +0800

    AVL SEARCH TREE. take a long time

--- LeetCode/Tree/InvertBinaryTree.ts
+++ LeetCode/Tree/InvertBinaryTree.ts
@@ -2,151 +2,205 @@
 /**
  * 
  * Definition for a binary tree node.*/
-  class TreeNode {
-      val: number
-      left: TreeNode | null
-      right: TreeNode | null
-      parent: TreeNode| null
-      height: number = 0
-      constructor(val?: number, left?: TreeNode | null, right?: TreeNode | null) {
-          this.val = (val===undefined ? 0 : val)
-          this.left = (left===undefined ? null : left)
-          this.right = (right===undefined ? null : right)
-          
-      }            
+class TreeNode {
+    val: number
+    left: TreeNode | null
+    right: TreeNode | null
+    parent: TreeNode | null
+    height: number = 0
+    constructor(val?: number, left?: TreeNode | null, right?: TreeNode | null) {
+        this.val = (val === undefined ? 0 : val)
+        this.left = (left === undefined ? null : left)
+        this.right = (right === undefined ? null : right)
 
-      updateHeight(){
-          this.height = Math.max(this.left ? this.left.height: -1, 
-                                this.right ? this.right.height : -1) + 1; 
-      }
+    }
 
-      isBalance(){
+    updateHeight() {
+        this.height = Math.max(this.left ? this.left.height : -1,
+            this.right ? this.right.height : -1) + 1;
+    }
+
+    isBalance() {
         let leftHeight = this.left ? this.left.height : -1;
         let rightHeight = this.right ? this.right.height : -1;
-        if(leftHeight - rightHeight == 2 ||
-            rightHeight - leftHeight == 2){
+        if (leftHeight - rightHeight == 2 ||
+            rightHeight - leftHeight == 2) {
             return false;
         }
-        else{
+        else {
             return true;
         }
-      }
-      
-      isLeftHeavy(){
-        return (this.left ? this.left.height : -1) > (this.right ? this.right.height: -1);
-      }
+    }
 
-      isRightHeavy(){
-        return (this.left ? this.left.height : -1) < (this.right ? this.right.height : -1);
-      }
-  }
- 
+    isLeftHeavy() {
+        return (this.left ? this.left.height : -1) > (this.right ? this.right.height : -1);
+    }
+
+    isRightHeavy() {
+        return (this.right ? this.right.height : -1) > (this.left ? this.left.height : -1);
+    }
+}
+
 
 function invertTree(root: TreeNode | null): TreeNode | null {
     return null;
 };
 
 
-class ContainerTreeNode{
+class ContainerTreeNode {
     head: TreeNode;
 
-    constructor(){}
+    constructor() { }
 
-    insert(val: number, treeNode: TreeNode): TreeNode{     
-        if(!this.head){
+    insert(val: number, treeNode: TreeNode): TreeNode {
+        if (!this.head) {
             this.head = new TreeNode(val);
             return this.head;
-        }           
-        else
-        {
-            if(val <= treeNode.val){
-                if(treeNode.left){
+        }
+        else {
+            if (val <= treeNode.val) {
+                if (treeNode.left) {
                     return this.insert(val, treeNode.left);
                 }
-                else{
+                else {
                     let newNode = new TreeNode(val);
                     treeNode.left = newNode;
                     newNode.parent = treeNode;
                     return newNode;
                 }
             }
-            else{
-                if(treeNode.right){
+            else {
+                if (treeNode.right) {
                     return this.insert(val, treeNode.right);
-                }else{
+                } else {
                     let newNode = new TreeNode(val);
                     treeNode.right = newNode;
                     newNode.parent = treeNode;
                     return newNode;
                 }
-            }            
+            }
         }
     }
 
-    rebalance(treeNode: TreeNode){
+    rebalance(treeNode: TreeNode) {
         // establish something 
         let currentNode = treeNode;
-        if(currentNode){
-            
+        if (currentNode) {
+
             let heightLeft = currentNode.left ? currentNode.left.height : -1;
             let heightRight = currentNode.right ? currentNode.right.height : -1;
-            if(treeNode.isBalance())
-            {
-                currentNode.updateHeight(); 
+            if (treeNode.isBalance()) {
+                currentNode.updateHeight();
                 this.rebalance(currentNode.parent);
             }
-            else{
+            else {
                 // rotation
-                if(currentNode.isLeftHeavy()){                    
-                    if(currentNode.left.isLeftHeavy()){
+                if (currentNode.isLeftHeavy()) {
+                    if (currentNode.left.isLeftHeavy()) {
                         let left = currentNode.left;
-                        let leftleft = currentNode.left.left;
                         // single rotation right
-                        left.parent = currentNode.parent;
+                        if(currentNode.parent){
+                            left.parent = currentNode.parent;
+                            currentNode.parent.left = left;
+                        }
+                        else{
+                            left.parent = currentNode.parent;
+                            this.head = left;
+                        }
+                        let tempLeftRight = left.right;
+                        currentNode.left = tempLeftRight;
+                        if(tempLeftRight){
+                            tempLeftRight.parent = currentNode
+                        }
+
                         left.right = currentNode;
                         currentNode.parent = left;
                     }
-                    else{
-                        // double rotation
+                    else {
+                           // double rotation 
                         let left = currentNode.left;
-                        let leftRight = currentNode.left.right;
-                        // first rotate
-                        currentNode.left = leftRight;
-                        leftRight.parent = currentNode;
-                        leftRight.left = left;
-                        left.parent = leftRight;
-
-                        // second rotate
-                        leftRight.parent = currentNode.parent;
-                        leftRight.right = currentNode;
-                        currentNode.parent = leftRight;
-                    }                    
-                }else{
-                    if(currentNode.right.isRightHeavy()){
-                        let right = currentNode.right;
-                        let rightright = currentNode.right.right;
-                        // single rotation left
-                        right.parent = currentNode.parent;
-                        right.left = currentNode;
-                        currentNode.parent = right
+                        let leftright = currentNode.left.right;
+                        let leftrightright = leftright.right;
+                        let leftrightleft = leftright.left;
+                        
+                        currentNode.left = leftright;
+                        leftright.left = left;
+                        leftright.right = currentNode;
+                        leftright.parent = currentNode.parent;
+                        if(currentNode.parent){
+                            currentNode.parent.left = leftright;
+                        }else{
+                            this.head = leftright;
+                        }
+                        if(leftrightright){
+                            currentNode.right = leftrightright;
+                            leftrightright.parent = currentNode
+                        }else{
+                            currentNode.right = leftrightright;
+                        }
+                        if(leftrightleft){                            
+                            left.right = leftrightleft;
+                            leftrightleft.parent = currentNode
+                        }else{
+                            left.right = leftrightleft
+                        }
                     }
-                    else{
-                        // double rotation 
+                } else {
+                    if (currentNode.right.isRightHeavy()) {
+                        let right = currentNode.right;
+                        // single rotation left
+                        if(currentNode.parent)
+                        {
+                            right.parent = currentNode.parent;
+                            currentNode.parent.right = right;
+                        }
+                        else{
+
+                            right.parent = currentNode.parent;
+                            this.head = right;
+                        }
+                        let tempRightLeft = right.left;                        
+                        currentNode.right = tempRightLeft;
+                        
+                        if(tempRightLeft){                            
+                            tempRightLeft.parent = currentNode; 
+                        }
+
+                        right.left = currentNode;
+                        currentNode.parent = right;
+                    }
+                    else {
+                            // double rotation 
                         let right = currentNode.right;
                         let rightleft = currentNode.right.left;
-                        // First rotate
+                        let rightleftleft = rightleft.left;
+                        let rightleftright = rightleft.right;
+                        
                         currentNode.right = rightleft;
-                        rightleft.parent = currentNode;
                         rightleft.right = right;
-                        right.parent = rightleft;
-                        // second rotation 
+                        rightleft.left = currentNode;
                         rightleft.parent = currentNode.parent;
-                        rightleft.right = currentNode;
-                        currentNode.parent = rightleft;
+                        if(currentNode.parent){
+                            currentNode.parent.right = rightleft;
+                        }else{
+                            this.head = rightleft;
+                        }
+                        if(rightleftleft){
+                            currentNode.right = rightleftleft;
+                            rightleftleft.parent = currentNode
+                        }else{
+                            currentNode.right = rightleftleft;
+                        }
+                        if(rightleftright){                            
+                            right.left = rightleftright;
+                            rightleftright.parent = currentNode
+                        }else{
+                            right.left = rightleftright
+                        }
                     }
                 }
             currentNode.updateHeight();
-            this.rebalance(currentNode.parent);    
+            this.rebalance(currentNode.parent);
             }
         }
     }
@@ -154,11 +208,19 @@ class ContainerTreeNode{
 
 
 let testContainerTreeNode = new ContainerTreeNode();
-var insertedNode = testContainerTreeNode.insert(1, testContainerTreeNode.head);
+var insertedNode = testContainerTreeNode.insert(9, testContainerTreeNode.head);
 // rebalance
+testContainerTreeNode.rebalance(insertedNode);
+insertedNode = testContainerTreeNode.insert(7, testContainerTreeNode.head);
+testContainerTreeNode.rebalance(insertedNode);
+insertedNode = testContainerTreeNode.insert(6, testContainerTreeNode.head);
+testContainerTreeNode.rebalance(insertedNode);
+insertedNode = testContainerTreeNode.insert(4, testContainerTreeNode.head);
+testContainerTreeNode.rebalance(insertedNode);
+insertedNode = testContainerTreeNode.insert(3, testContainerTreeNode.head);
 testContainerTreeNode.rebalance(insertedNode);
 insertedNode = testContainerTreeNode.insert(2, testContainerTreeNode.head);
 testContainerTreeNode.rebalance(insertedNode);
-insertedNode = testContainerTreeNode.insert(3, testContainerTreeNode.head);
+insertedNode = testContainerTreeNode.insert(1, testContainerTreeNode.head);
 testContainerTreeNode.rebalance(insertedNode);
 console.log(testContainerTreeNode);
