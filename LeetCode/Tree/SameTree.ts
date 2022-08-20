f66832190685018cc108effe0a5b4092f25e623f
namhoaingo <namhoaingo@gmail.com>
2022-08-20 22:53:40 +0800

    Get same tree in

--- LeetCode/Tree/SameTree.ts
+++ LeetCode/Tree/SameTree.ts
@@ -15,5 +15,20 @@
  */
 
 function isSameTree(p: TreeNode | null, q: TreeNode | null): boolean {
-
+    if(p == null && q==null){
+        return true;
+    }
+    else{
+        let rightSameTree = isSameTree(p.right, q.right);
+        let leftSameTree = isSameTree(p.left, q.left);
+        if(rightSameTree && leftSameTree && q.val == q.val){
+            return true;
+        }
+        else
+        {
+            return false;
+        }
+    }
 };
+//Runtime: 109 ms, faster than 31.85% of TypeScript online submissions for Same Tree.
+//Memory Usage: 44.2 MB, less than 84.34% of TypeScript online submissions for Same Tree.
