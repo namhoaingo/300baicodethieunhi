313f8aeb5630675281fdf9d4d65f51a30a42c867
namhoaingo <namhoaingo@gmail.com>
2022-08-20 22:06:58 +0800

    Diametter complete

--- LeetCode/Tree/DiameterOfBTree.ts
+++ LeetCode/Tree/DiameterOfBTree.ts
@@ -15,5 +15,27 @@
  */
 
 function diameterOfBinaryTree(root: TreeNode | null): number {
-
+   let maxDia = 0;
+   return findCurrentMaxWidthAndMaxHeight(root, maxDia)[0];
 };
+
+
+function findCurrentMaxWidthAndMaxHeight(treeNode: TreeNode, currentMax: number){
+    if(!treeNode){
+        return [-1, -1];
+        // width + height
+    }
+    else{
+        let currentWidth_Height_Left = findCurrentMaxWidthAndMaxHeight(treeNode.left, currentMax);
+        let currentWidth_Height_Right = findCurrentMaxWidthAndMaxHeight(treeNode.right, currentMax);
+        let currentWidth = currentWidth_Height_Left[1] + currentWidth_Height_Right[1] + 2;
+        let currentMaxheight =   Math.max(currentWidth_Height_Left[1] + 1, currentWidth_Height_Right[1] + 1)
+        return [
+            Math.max(currentWidth_Height_Left[0], currentWidth_Height_Right[0], currentWidth)
+            ,
+            currentMaxheight           
+            ];
+    }
+}
+//Runtime: 104 ms, faster than 73.02% of TypeScript online submissions for Diameter of Binary Tree.
+//Memory Usage: 47.5 MB, less than 19.78% of TypeScript online submissions for Diameter of Binary Tree.
