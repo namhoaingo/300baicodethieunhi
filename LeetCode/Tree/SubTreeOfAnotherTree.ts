c5264b957ccc5acff7f798fad60ca59253d4b1f3
namhoaingo <namhoaingo@gmail.com>
2022-08-21 12:26:31 +0800

    Cannot get Sub tree work. Yuckkk

--- LeetCode/Tree/SubTreeOfAnotherTree.ts
+++ LeetCode/Tree/SubTreeOfAnotherTree.ts
@@ -1,26 +1,44 @@
 //https://leetcode.com/problems/subtree-of-another-tree/
 /**
  * Definition for a binary tree node.
- * class TreeNode {
+ * class TreeNodeSubTree {
  *     val: number
- *     left: TreeNode | null
- *     right: TreeNode | null
- *     constructor(val?: number, left?: TreeNode | null, right?: TreeNode | null) {
+ *     left: TreeNodeSubTree | null
+ *     right: TreeNodeSubTree | null
+ *     constructor(val?: number, left?: TreeNodeSubTree | null, right?: TreeNodeSubTree | null) {
  *         this.val = (val===undefined ? 0 : val)
  *         this.left = (left===undefined ? null : left)
  *         this.right = (right===undefined ? null : right)
  *     }
  * }
  */
-
 function isSubtree(root: TreeNode | null, subRoot: TreeNode | null): boolean {
-    if(root == null && subRoot == null){
+    if(isSameTree(root,subRoot)){
         return true;
     }
     else{
-        if(root.val != subRoot.val){
-            // advance root
-            
+        return isSameTree(root.left, subRoot) || isSameTree(root.right, subRoot);
+    }
+};
+
+
+function isSameTree(p: TreeNode | null, q: TreeNode | null): boolean {
+    if(p == null && q==null){
+        return true;
+    }else if ((q == null && p != null) || (q !=null && p == null)) {
+        return false;
+    }
+    else{
+        let rightSameTree = isSameTree(p.right, q.right);
+        let leftSameTree = isSameTree(p.left, q.left);
+        if(rightSameTree && leftSameTree && p.val == q.val){
+            return true;
+        }
+        else
+        {
+            return false;
         }
     }
 };
+
+ 
